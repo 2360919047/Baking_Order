c018ea8cf9b004cee3b616d837df18cd21feec26
2360919047 <2360919047@qq.com>
2021-04-12 19:52:39 +0800

    第一次提交

--- Baking_Order/src/main/java/com/rj/bd/user/controller/UserController.java
+++ Baking_Order/src/main/java/com/rj/bd/user/controller/UserController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.rj.bd.user.service.IUserService;
+
 @Controller
 @ResponseBody
 @RequestMapping("/user")
 public class UserController {
 	
-	
+	public IUserService userService;
 }
